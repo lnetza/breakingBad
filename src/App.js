bd19af5def41cadecbf8b0a6b3fc6cbde11f5712
lnetza <l.lopeznetza@gmail.com>
2020-08-26 21:45:27 -0500

    se agrega @emotion y respuesta api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import styled from '@emotion/styled';
 
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
+
+/*Sin await
+const consultarAPI=()=>{
+  const resultado = fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+  const frase = resultado.then( respuesta=> respuesta.json());
+  frase.then(resultado => console.log(resultado));
+  
+}
+*/
+/**Con await */
+const consultarAPI= async()=>{
+  const api =await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+  const frase = await api.json()
+  console.log(frase[0]);
+  
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   );
 }
 
 export default App;
+//instalar emotion: npm i @emotion/core @emotion/styled
